b076e845d34deb46b9943a68801f80626e2f880e
enriqueM01 <emendezsmith@gmail.com>
2021-09-17 12:02:52 -0400

    Cambio de logs de servicio: usuario ha finalizado servicio

--- pana-beacon-project-api/app/Http/Controllers/ServiceController.php
+++ pana-beacon-project-api/app/Http/Controllers/ServiceController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
 class ServiceController extends Controller
 {
     public function inicioservicio(Request $request)
@@ -12,26 +9,24 @@ class ServiceController extends Controller
         $user = $request->user();
         $hora = $request['hora'];
 
-        Log::info("El usuario $user->name inicia sesion. Hora de registro en App $hora");
+        Log::info("El usuario $user->name inicia servicio. Hora de registro en App $hora");
 
         return response()->json([
             'message' => "Inicio de servicio registrado"
         ]);
     }
-
     public function finservicio(request $request)
     {
         $user = $request->user();
         $hora = $request['hora'];
 
-        Log::info("El usuario $user->name finaliza sesion. Hora de registro en App $hora");
+        Log::info("El usuario $user->name finaliza servicio. Hora de registro en App $hora");
 
         return response()->json([
             'message'=> "Fin de servicio registrado"
         ]);
 
     }
-
     public function ayuda(request $request)
     {
         $user = $request->user();
@@ -43,8 +38,6 @@ class ServiceController extends Controller
             'message'=> "Ayuda notificada al Centro de Control"
         ]);
     }
-
-
 
 
 }
